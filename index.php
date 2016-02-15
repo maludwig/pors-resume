0593a74cb2565391677b3a1ddf86c77af7930be0
Emily Stener <emily.stener@live.com>
2016-02-15 02:05:06 -0700

    added scaffold

--- index.php
+++ index.php
@@ -1,30 +1,47 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Mike Pors</title>
 	<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<link href='https://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="css/style.css" />
-	<script src="main.js"></script>
-	<script type="text/javascript">
-		$(function() {
-			$("#first").ring();
-		});
-	</script>
+
 </head>
 
 <body>
-	<header>
-	</header>
-	<div class="container">
-		<canvas id="first" class="ring" value="14" width="400" height="400"></canvas>
-	</div>
-	<footer>
-	</footer>
-</body>
-</html>
+	<div class="container-fluid">
+		<header class="text-center">
+			<h2 id="mike" class="title">MIKE</h2>
+			<hr />
+			<h2 class="title">PORS</h2>
 
+			<div id="contact">
+				<span class="glyphicon glyphicon-envelope"></span><a href="mailto:mike.pors@gmail.com">e-mail</a>
+				<span class="glyphicon glyphicon-phone"></span><a href="tel:403-477-9974">403-477-9974</a>
+				<br />
+				<button type="button" class="btn btn-default"><span class="glyphicon glyphicon-download"></span> PDF</button>
+			</div>
+		</header>
+		<div class="col-xs-12 col-md-6" id="bio"></div>
+		<div class="col-xs-12 col-md-6" id="certs"></div>
+		<div class="col-xs-12 col-md-6" id="dev"></div>
+		<div class="col-xs-12 col-md-6" id="tech"></div>
+		<div class="col-xs-12 col-md-6" id="history"></div>
+		<div class="col-xs-12 col-md-6" id="awards"></div>
+		<footer>
+		</footer>
+	</div>
+</body>
+
+</html>
