b2fb7623b0766da67db8cbed5f8907cf33ffa6f5
Mitchell Ludwig <mitchell.ludwig@gmail.com>
2016-02-15 02:00:20 -0700

    Made rings

--- index.php
+++ index.php
@@ -9,10 +9,14 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
 
 	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<script src="js/linearalgebra-1.5.js"></script>
+	<script src="js/color.1.3.js"></script>
+	<script src="js/jquery-center.1.2.js"></script>
 	<script src="main.js"></script>
 	<script type="text/javascript">
 		$(function() {
-			$("#first").ring();
+			$(".ring").ring();
+			$(".skill p").center();
 		});
 	</script>
 </head>
@@ -21,7 +25,85 @@
 	<header>
 	</header>
 	<div class="container">
-		<canvas id="first" class="ring" value="14" width="400" height="400"></canvas>
+		<div class="col-md-6">
+			<div class="row">
+				<h3>Development</h3>
+				<div class="skill">
+					<p>Java</p>
+					<img src="img/cert.png">
+					<canvas class="ring" data-fill="24" width="100" height="100"></canvas>
+				</div>
+				<div class="skill">
+					<p>Python</p>
+					<canvas class="ring" data-fill="24" width="100" height="100"></canvas>
+				</div>
+				<div class="skill">
+					<p>XML</p>
+					<canvas class="ring" data-fill="23" width="100" height="100"></canvas>
+				</div>
+				<div class="skill">
+					<p>JS</p>
+					<canvas class="ring" data-fill="20" width="100" height="100"></canvas>
+				</div>
+			</div>
+			<div class="row">
+				<div class="skill">
+					<p>C#</p>
+					<canvas class="ring" data-fill="20" width="100" height="100"></canvas>
+				</div>
+				<div class="skill">
+					<p>SQL</p>
+					<canvas class="ring" data-fill="18" width="100" height="100"></canvas>
+				</div>
+				<div class="skill">
+					<p>HTML</p>
+					<canvas class="ring" data-fill="12" width="100" height="100"></canvas>
+				</div>
+				<div class="skill">
+					<p>CSS</p>
+					<canvas class="ring" data-fill="6" width="100" height="100"></canvas>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="row">
+				<h3>Technologies</h3>
+				<div class="skill">
+					<p>Android</p>
+					<canvas class="ring" data-fill="23" width="100" height="100"></canvas>
+				</div>
+				<div class="skill">
+					<p>LaTeX</p>
+					<canvas class="ring" data-fill="21" width="100" height="100"></canvas>
+				</div>
+				<div class="skill">
+					<p>Windows</p>
+					<canvas class="ring" data-fill="20" width="100" height="100"></canvas>
+				</div>
+				<div class="skill">
+					<p>Git</p>
+					<canvas class="ring" data-fill="15" width="100" height="100"></canvas>
+				</div>
+			</div>
+			<div class="row">
+				<div class="skill">
+					<p>Visual<br />Studio</p>
+					<canvas class="ring" data-fill="13" width="100" height="100"></canvas>
+				</div>
+				<div class="skill">
+					<p>Django</p>
+					<canvas class="ring" data-fill="10" width="100" height="100"></canvas>
+				</div>
+				<div class="skill">
+					<p>Cloud</p>
+					<canvas class="ring" data-fill="7" width="100" height="100"></canvas>
+				</div>
+				<div class="skill">
+					<p>Linux</p>
+					<canvas class="ring" data-fill="6" width="100" height="100"></canvas>
+				</div>
+			</div>
+		</div>
 	</div>
 	<footer>
 	</footer>
--- main.js
+++ main.js
@@ -1,8 +1,79 @@
+//Requires linearalgebra-1.5
+/*jshint -W117 */
+
 (function($){
-	$.fn.ring = function (options) {
-		alert(this.attr("id"));
+	$.fn.ring = function (opt) {
+		return this.each(function (i) {
+			var $this = $(this);
+			var options = {
+				rainbow: false,
+				color: new Color('#fa0000'),
+				animated: true,
+				duration: 2000
+			};
+			$.extend(options, opt);
+			options.color = new Color(options.color);
+			$this.data("options",options);
+			$this.addClass("ring");
+			if (options.animated) {
+				$this.data("fill-step",1);
+				var interval = setInterval(function() {
+					var fill_step = $this.data("fill-step");
+					$this.ringDraw(fill_step);
+					$this.data("fill-step", fill_step + 1);
+					if ($this.data("fill") <= fill_step) {
+						clearInterval($this.data("interval"));
+					}
+				}, options.duration / 24);
+				$this.data("interval",interval);
+			} else {
+				$this.ringDraw($this.data("fill"));
+			}
+		});
 	};
-	$.fn.ringDraw = function (options) {
-		alert("Drawing: " + this.attr("id") + this.val());
+	$.fn.ringDraw = function (fill) {
+		var canvas = this[0];
+		var ctx = canvas.getContext('2d');
+		var midpoint = new Point(canvas.width/2, canvas.height/2);
+		var pointer = new Point(canvas.width/2 + canvas.width/40, canvas.height/10);
+		var left_tail = new Point(canvas.width/2 - canvas.width/40, canvas.height/10 - canvas.height/20);
+		var right_tail = new Point(canvas.width/2 - canvas.width/40, canvas.height/10 + canvas.height/20);
+		var i;
+		var options = this.data("options");
+		var color = options.color;
+
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.lineCap = 'round';
+		ctx.lineWidth = canvas.width / 60;
+		ctx.strokeStyle = color;
+
+		for (i=0;i<fill;i++) {
+			ctx.beginPath();
+			ctx.moveTo(left_tail.x, left_tail.y);
+			ctx.lineTo(pointer.x, pointer.y);
+			ctx.moveTo(pointer.x, pointer.y);
+			ctx.lineTo(right_tail.x, right_tail.y);
+			ctx.stroke();
+
+			pointer = pointer.rotate(Math.PI/12,midpoint);
+			left_tail = left_tail.rotate(Math.PI/12,midpoint);
+			right_tail = right_tail.rotate(Math.PI/12,midpoint);
+			if (options.rainbow) color = color.spin(15);
+			ctx.strokeStyle = color;
+		}
+		ctx.strokeStyle = '#ffffff';
+		for (;i<24;i++) {
+			ctx.beginPath();
+			ctx.moveTo(left_tail.x, left_tail.y);
+			ctx.lineTo(pointer.x, pointer.y);
+			ctx.moveTo(pointer.x, pointer.y);
+			ctx.lineTo(right_tail.x, right_tail.y);
+			ctx.stroke();
+
+			pointer = pointer.rotate(Math.PI/12,midpoint);
+			left_tail = left_tail.rotate(Math.PI/12,midpoint);
+			right_tail = right_tail.rotate(Math.PI/12,midpoint);
+		}
+
 	};
 })(jQuery);// JavaScript Document
